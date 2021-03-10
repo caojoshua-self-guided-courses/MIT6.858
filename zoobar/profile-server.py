8df000bb9ddb7b401a1f1a3b9635c56b6fb753f2
caojoshua <cao.joshua@yahoo.com>
2021-03-10 00:03:00 -0800

    Add profile-server service.

--- zoobar/profile-server.py
+++ zoobar/profile-server.py
@@ -56,7 +56,7 @@ def run_profile(pcode, profile_api_client):
 
 class ProfileServer(rpclib.RpcServer):
     def rpc_run(self, pcode, user, visitor):
-        uid = 0
+        uid = 61013
 
         userdir = '/tmp'
 
